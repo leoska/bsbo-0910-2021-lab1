22d99034a7383516046728a2edb3321d99c6270e
Leonid <Leo77551@gmail.com>
2022-11-05 11:43:08 +0300

    first commit
    Pointers, Queue only head, Bubble sort

---
+++ BSBO-0910-2021-lab1/Program.cs
@@ -0,0 +1,38 @@
+﻿// Pointers, Queue only head, Bubble sort
+using System;
+
+using BSBO_0910_2021_lab1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Queue queue1 = new Queue();
+        Random random = new Random();
+        int N = 100;
+        for (int i = 0; i < N; i++)
+        {
+            queue1.Push(new Item(random.Next(1, 100)));
+        }
+        queue1.Print();
+        
+
+        for (int i = 0; i < N; i++)
+        {
+            bool flag = false;
+            for (int j = 0; j < N-1-i; j++)
+            {
+                int a = queue1.Get(j);
+                int b = queue1.Get(j+1);
+                if (a > b)
+                {
+                    flag = true;
+                    queue1.Set(j, b);
+                    queue1.Set(j + 1, a);
+                }
+            }
+            if (!flag) break;
+        }
+
+        queue1.Print();
+    }
+}
---
+++ BSBO-0910-2021-lab1/Queue.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BSBO_0910_2021_lab1
+{
+    internal class Queue
+    {
+
+        public Item head = null;
+
+        public void Push(Item item)
+        {
+            if (head == null)
+            { 
+                head = item;
+            }
+            else
+            {
+                Item current = head;
+                while( current.next != null)
+                {
+                    current = current.next; 
+                }
+                current.next = item;
+            }
+            item.next = null;
+        }
+
+        public Item Pop()
+        {
+            if (head == null)
+            {
+                throw new Exception("Head is null in Pop method");
+            }
+            else
+            {
+                Item current = head;
+                head = head.next;
+                return current;
+            }
+        }
+
+        public int Get(int idx)
+        {
+            Item oldHead = head;
+            for (int i = 0; i < idx; i++)
+                Push(Pop());
+            
+            int result = head.value;
+            while (head != oldHead)
+                Push(Pop());
+
+            return result;
+        }
+
+        public void Set(int idx, int newVal)
+        {
+            Item oldHead = head;
+            for (int i = 0; i < idx; i++)
+                Push(Pop());
+
+            head.value = newVal;
+            while (head != oldHead)
+                Push(Pop());
+        }
+
+        public void Print()
+        {
+            Item current = head;
+            while (current != null)
+            {
+                Console.Write(current.value + " ");
+                current = current.next;
+            }
+            Console.WriteLine();
+        }
+    }
+}
